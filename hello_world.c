c225ead6675acfad3f596b15ec6614e5a83b22e5
unknown <a084vgu@yamaguchi-u.ac.jp>
2020-10-16 12:37:49 +0900

    ハロー追加

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+    char *str1 = "Hello";
+    char *str2 = "World";
+    char chr1 = '!';
+ 
+     // 文字および文字列
+    printf("%s %s%c\n", str1, str2, chr1);
+ 
+
+  
+    return 0;
+}
