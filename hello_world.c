89c08011030d65df1a822fe1c7019631b563539e
unknown <a084vgu@yamaguchi-u.ac.jp>
2020-10-16 12:40:32 +0900

    趣味追加

--- hello_world.c
+++ hello_world.c
@@ -4,9 +4,11 @@ int main(void) {
     char *str1 = "Hello";
     char *str2 = "World";
     char chr1 = '!';
+char *str3 = "My hobby is ..........";
  
      // 文字および文字列
     printf("%s %s%c\n", str1, str2, chr1);
+printf("%s");
  
 
   
